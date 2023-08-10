ecef96715087ffedd3564c9d34c43093de54fbb8
Mukiibi Robert <onlineproducts58@gmail.com>
2023-08-10 14:16:06 +0000

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,7 @@
  * main - this is a function
  * Return: is 0 always
 */
-int main(void)
-{
-puts("\""Programming is like building a multilingual puzzle");
+int main(void) {
+puts("\"Programming is like building a multilingual puzzle");
 return 0;
 }
