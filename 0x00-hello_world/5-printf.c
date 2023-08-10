cb112ba9aaee993b746ee4d9eaf3fb101f2370ff
Mukiibi Robert <onlineproducts58@gmail.com>
2023-08-10 14:36:51 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this is a function
+ * Return: is always 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
