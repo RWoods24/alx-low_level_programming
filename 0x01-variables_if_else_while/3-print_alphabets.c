c7cc575f680fc6d51aacaa16fe512bc9215a1a79
Mukiibi Robert <onlineproducts58@gmail.com>
2023-08-11 12:22:28 +0000

    3-print_alphabets.

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - it is the main function
+ * Return: always 0
+ */
+
+int main(void)
+{
+int i;
+	for (i = 'a' ; i <= 'z' ; i++)
+	putchar(i);
+for (i = 'A' ; i <= 'Z' ; i++)
+	putchar(i);
+putchar('\n');
+return (0);
+}
