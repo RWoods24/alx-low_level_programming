5f2fc9ea0ac5da85b27fda2f8a8545ced803979b
Mukiibi Robert <onlineproducts58@gmail.com>
2023-08-30 09:59:46 +0000

    101-wildcmp.c

--- 0x08-recursion/101-main.c
+++ 0x08-recursion/101-main.c
@@ -10,6 +10,14 @@ int main(void)
 {
 	int r;
 
+
+	r = wildcmp("main.c", "*.c");
+	printf("%d\n", r);
+	r = wildcmp("main.c", "m*a*i*n*.*c*");
+	printf("%d\n", r);
+	r = wildcmp("main.c", "main.c");
+	printf("%d\n", r);
+	r = wildcmp("main.c", "m*c");
 	printf("%d\n", r);
 	r = wildcmp("main.c", "ma********************************c");
 	printf("%d\n", r);
