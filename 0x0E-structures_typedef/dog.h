7f31fd4528fc72cd4a11c0fb521d04735b1605aa
Mukiibi Robert <onlineproducts58@gmail.com>
2023-09-11 11:53:48 +0000

    4-new_dog.c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,19 +17,17 @@
  */
 
 struct dog
-
 {
 	char *name;
 
 	float age;
 
 	char *owner;
-
 };
 
+
 /**
- * dog_t - typedef for struct dog
- *
+ * *dog_t - typedef for struct dog
  */
 
 typedef struct dog dog_t;
